42ce1b2eea31fea9c9b799c0b4c9d54a48201347
rachelyr <rachel.yorke225@gmail.com>
2025-03-05 11:32:21 +0530

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Customers() {
+  return (
+    <p>
+      Customer Page
+    </p>
+  )
+}
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Invoices() {
+  return (
+    <p>
+      Invoices Page
+    </p>
+  )};
+
+export default Invoices;
